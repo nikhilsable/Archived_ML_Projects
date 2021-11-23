f622b2a8894c72113a917d9dcae8d4222688ebc2
Nikhil <nikhilsable@gmail.com>
2021-11-22 16:44:14 -0800

    black test

--- outlier_detection_example.py
+++ outlier_detection_example.py
@@ -1,22 +1,26 @@
 import sys
+
 assert sys.version_info >= (3, 5)
 
 # Scikit-Learn ≥0.20 is required
 import sklearn
+
 assert sklearn.__version__ >= "0.20"
-from sklearn.cluster import KMeans
 import seaborn as sns
-from sklearn.pipeline import Pipeline
-from sklearn.preprocessing import StandardScaler
 
 # TensorFlow ≥2.0 is required
 import tensorflow as tf
+from sklearn.cluster import KMeans
+from sklearn.pipeline import Pipeline
+from sklearn.preprocessing import StandardScaler
 from tensorflow import keras
+
 assert tf.__version__ >= "2.0"
+
+import os
 
 # Common imports
 import numpy as np
-import os
 import pandas as pd
 
 # to make this notebook's output stable across runs
@@ -25,15 +29,17 @@ tf.random.set_seed(42)
 
 import matplotlib as mpl
 import matplotlib.pyplot as plt
-mpl.rc('axes', labelsize=14)
-mpl.rc('xtick', labelsize=12)
-mpl.rc('ytick', labelsize=12)
+
+mpl.rc("axes", labelsize=14)
+mpl.rc("xtick", labelsize=12)
+mpl.rc("ytick", labelsize=12)
 
 # Where to save the figures
 PROJECT_ROOT_DIR = "."
 CHAPTER_ID = "anomaly_detection_clustering"
 IMAGES_PATH = os.path.join(PROJECT_ROOT_DIR, "images", CHAPTER_ID)
 os.makedirs(IMAGES_PATH, exist_ok=True)
+
 
 def save_fig(fig_id, tight_layout=True, fig_extension="png", resolution=300):
     path = os.path.join(IMAGES_PATH, fig_id + "." + fig_extension)
@@ -42,79 +48,90 @@ def save_fig(fig_id, tight_layout=True, fig_extension="png", resolution=300):
         plt.tight_layout()
     plt.savefig(path, format=fig_extension, dpi=resolution)
 
-filename = r'data_sources/raw_pp_seed_data_9739_outlier_analysis.csv'
-df = pd.read_csv(filename, index_col='time')
+
+filename = r"data_sources/raw_pp_seed_data_9739_outlier_analysis.csv"
+df = pd.read_csv(filename, index_col="time")
 df.index = pd.to_datetime(df.index.values)
 
-#Reindex to identify missing values
-#df = df_reindexer(df, timesteps='H')
+# Reindex to identify missing values
+# df = df_reindexer(df, timesteps='H')
 
-#handle missing values
+# handle missing values
 df = df.ffill()
 
-df.columns = ['peakpower_on', 'pk2pk', 'prelase', 'pzt', 'pk2pk_3sig', 'peak_power_off_3sig', 'pk2pk_oos', 'peakpower_off']
+df.columns = [
+    "peakpower_on",
+    "pk2pk",
+    "prelase",
+    "pzt",
+    "pk2pk_3sig",
+    "peak_power_off_3sig",
+    "pk2pk_oos",
+    "peakpower_off",
+]
 
-#context filtering
+# context filtering
 uel = 650
 lel = 100
 
 df = df[(df.peakpower_off <= uel) & (df.peakpower_off >= lel)]
 
-df.prelase = df.prelase * 1000000 #converting microseconds to seconds
+df.prelase = df.prelase * 1000000  # converting microseconds to seconds
 
 df.pk2pk_3sig = df[df.pk2pk_3sig <= 25]
 
 df = df.dropna()
 
-#setup lookback period (in days)
+# setup lookback period (in days)
 lookback = 180
-df = df.loc[(df.index.max()-pd.Timedelta(days=lookback)):df.index.max()]
+df = df.loc[(df.index.max() - pd.Timedelta(days=lookback)) : df.index.max()]
 
-#intro plots
-#sns.pairplot(df)
+# intro plots
+# sns.pairplot(df)
 
-#prep data for algo
+# prep data for algo
 raw_X = df.iloc[:, :-1].values
 
-num_pipeline = Pipeline([
-        ('std_scaler', StandardScaler()),
-    ])
+num_pipeline = Pipeline([("std_scaler", StandardScaler()),])
 
 df_num_tr = num_pipeline.fit_transform(raw_X)
 
-#Setup training data
+# Setup training data
 X = df_num_tr[:, :-1]
 y = df.iloc[:, -1].values
 
 
-#Setup for K-means clustering
+# Setup for K-means clustering
 no_of_clusters = 2
 k = no_of_clusters
 kmeans = KMeans(n_clusters=k, random_state=42)
 y_pred = kmeans.fit_predict(X)
 
-#cluster centers
+# cluster centers
 print("cluster centers = " + str(kmeans.cluster_centers_))
 
-#index of the cluster that instance gets assigned to
+# index of the cluster that instance gets assigned to
 print("cluster labels = " + str(kmeans.labels_))
 
 df = df.reset_index()
-df['cluster'] = pd.Series(y_pred)
-df = df.set_index('index')
+df["cluster"] = pd.Series(y_pred)
+df = df.set_index("index")
 
-total_plot = sns.pairplot(df, hue='cluster')
-total_plot.fig.suptitle(("Lookback : " + str(lookback) + " --> "+ filename), fontsize=14, y=1.08)
+total_plot = sns.pairplot(df, hue="cluster")
+total_plot.fig.suptitle(("Lookback : " + str(lookback) + " --> " + filename), fontsize=14, y=1.08)
 save_fig("pairplot")
 
-df[['cluster']].plot(alpha=0.2, style='8', figsize = (18,13), title=("Lookback : " + str(lookback) + " --> " + "Clusters : " + filename))
+df[["cluster"]].plot(
+    alpha=0.2, style="8", figsize=(18, 13), title=("Lookback : " + str(lookback) + " --> " + "Clusters : " + filename)
+)
 save_fig("cluster_over_time")
-#df.cluster.plot(style='8', figsize=(18,13), title = "Clusters")
+# df.cluster.plot(style='8', figsize=(18,13), title = "Clusters")
 
-#df.pk2pk_oos.plot(style='8', figsize=(18,13))
-#plt.show()
+# df.pk2pk_oos.plot(style='8', figsize=(18,13))
+# plt.show()
 
-#df.pzt.plot(style='8', figsize=(18,13))
-#plt.show()
+# df.pzt.plot(style='8', figsize=(18,13))
+# plt.show()
 
-#sns.pairplot(df[['cluster', 'pzt']], hue='cluster', ).figsize=(18,13)
+# sns.pairplot(df[['cluster', 'pzt']], hue='cluster', ).figsize=(18,13)
+
--- rnn_example.py
+++ rnn_example.py
@@ -1,18 +1,22 @@
 import sys
+
 assert sys.version_info >= (3, 5)
 
 # Scikit-Learn ≥0.20 is required
 import sklearn
+
 assert sklearn.__version__ >= "0.20"
 
 # TensorFlow ≥2.0 is required
 import tensorflow as tf
 from tensorflow import keras
+
 assert tf.__version__ >= "2.0"
+
+import os
 
 # Common imports
 import numpy as np
-import os
 import pandas as pd
 
 # to make this notebook's output stable across runs
@@ -21,15 +25,17 @@ tf.random.set_seed(42)
 
 import matplotlib as mpl
 import matplotlib.pyplot as plt
-mpl.rc('axes', labelsize=14)
-mpl.rc('xtick', labelsize=12)
-mpl.rc('ytick', labelsize=12)
+
+mpl.rc("axes", labelsize=14)
+mpl.rc("xtick", labelsize=12)
+mpl.rc("ytick", labelsize=12)
 
 # Where to save the figures
 PROJECT_ROOT_DIR = "."
 CHAPTER_ID = "rnn"
 IMAGES_PATH = os.path.join(PROJECT_ROOT_DIR, "images", CHAPTER_ID)
 os.makedirs(IMAGES_PATH, exist_ok=True)
+
 
 def save_fig(fig_id, tight_layout=True, fig_extension="png", resolution=300):
     path = os.path.join(IMAGES_PATH, fig_id + "." + fig_extension)
@@ -38,32 +44,36 @@ def save_fig(fig_id, tight_layout=True, fig_extension="png", resolution=300):
         plt.tight_layout()
     plt.savefig(path, format=fig_extension, dpi=resolution)
 
+
 # ### Generate the Dataset
 def generate_time_series(batch_size, n_steps):
     freq1, freq2, offsets1, offsets2 = np.random.rand(4, batch_size, 1)
     time = np.linspace(0, 1, n_steps)
     series = 0.5 * np.sin((time - offsets1) * (freq1 * 10 + 10))  #   wave 1
-    series += 0.2 * np.sin((time - offsets2) * (freq2 * 20 + 20)) # + wave 2
-    series += 0.1 * (np.random.rand(batch_size, n_steps) - 0.5)   # + noise
+    series += 0.2 * np.sin((time - offsets2) * (freq2 * 20 + 20))  # + wave 2
+    series += 0.1 * (np.random.rand(batch_size, n_steps) - 0.5)  # + noise
     return series[..., np.newaxis].astype(np.float32)
 
-def chop_time_series(df, n_steps, batch_size = None):
-    max_samples = n_steps * int(len(df)/n_steps)
-    batch_size = int(len(df)/n_steps) if batch_size == None else batch_size
+
+def chop_time_series(df, n_steps, batch_size=None):
+    max_samples = n_steps * int(len(df) / n_steps)
+    batch_size = int(len(df) / n_steps) if batch_size == None else batch_size
     truncated_df = df.iloc[:max_samples, :]
     series_ts = truncated_df.values.reshape(batch_size, n_steps, 1)
 
     return batch_size, series_ts.astype(np.float32)
 
-#Generate Plots
+
+# Generate Plots
 def plot_multiple_forecasts(X, Y, Y_pred):
     n_steps = X.shape[1]
     ahead = Y.shape[1]
     plot_series(X[0, :, 0])
     plt.plot(np.arange(n_steps, n_steps + ahead), Y[0, :, 0], "ro-", label="Actual")
     plt.plot(np.arange(n_steps, n_steps + ahead), Y_pred[0, :, 0], "bx-", label="Forecast")
-    #plt.axis([0, n_steps + ahead, -1, 1])
+    # plt.axis([0, n_steps + ahead, -1, 1])
     plt.legend(fontsize=14)
+
 
 def plot_series(series, y=None, y_pred=None, x_label="$t$", y_label="$x(t)$"):
     plt.plot(series, ".-")
@@ -77,24 +87,29 @@ def plot_series(series, y=None, y_pred=None, x_label="$t$", y_label="$x(t)$"):
     if y_label:
         plt.ylabel(y_label, fontsize=16, rotation=0)
     plt.hlines(0, 0, 100, linewidth=1)
-    #plt.axis([0, n_steps + 1, -1, 1])
+    # plt.axis([0, n_steps + 1, -1, 1])
+
 
 def plot_learning_curves(loss, val_loss):
     plt.plot(np.arange(len(loss)) + 0.5, loss, "b.-", label="Training loss")
     plt.plot(np.arange(len(val_loss)) + 1, val_loss, "r.-", label="Validation loss")
     plt.gca().xaxis.set_major_locator(mpl.ticker.MaxNLocator(integer=True))
-    #plt.axis([1, 20, 0, 0.05])
+    # plt.axis([1, 20, 0, 0.05])
     plt.legend(fontsize=14)
     plt.xlabel("Epochs")
     plt.ylabel("Loss")
     plt.grid(True)
 
-def df_reindexer(df, timesteps='H'):
-    new_idx = pd.date_range(start=df.index.min().tz_localize(None), end=df.index.max().tz_localize(None), freq=timesteps, tz='UTC')
+
+def df_reindexer(df, timesteps="H"):
+    new_idx = pd.date_range(
+        start=df.index.min().tz_localize(None), end=df.index.max().tz_localize(None), freq=timesteps, tz="UTC"
+    )
     df_reindexed = pd.DataFrame(index=new_idx)
-    df_reindexed = df.join(df_reindexed, how='outer')
+    df_reindexed = df.join(df_reindexed, how="outer")
 
     return df_reindexed
+
 
 # %%
 np.random.seed(42)
@@ -103,21 +118,21 @@ lookforward = 50
 n_steps = 50
 
 
-df = pd.read_csv('data_sources/yr_sensor_data_test.csv', index_col='time')
+df = pd.read_csv("data_sources/yr_sensor_data_test.csv", index_col="time")
 df.index = pd.to_datetime(df.index.values)
 
-#Reindex to identify missing values
-df = df_reindexer(df, timesteps='H')
+# Reindex to identify missing values
+df = df_reindexer(df, timesteps="H")
 
-#handle missing values
+# handle missing values
 df = df.ffill()
 
-#series = generate_time_series(10000, n_steps + lookforward)
-batch_size, series= chop_time_series(df,n_steps+lookforward)
+# series = generate_time_series(10000, n_steps + lookforward)
+batch_size, series = chop_time_series(df, n_steps + lookforward)
 
-train_split = int(0.90*batch_size)
-valid_split = int(0.05*batch_size)
-test_split= int(0.05*batch_size)
+train_split = int(0.90 * batch_size)
+valid_split = int(0.05 * batch_size)
+test_split = int(0.05 * batch_size)
 
 # X_train = series[:48, :n_steps]
 # y_train = series[:48, -lookforward]
@@ -134,21 +149,17 @@ X_test = series[103:, :n_steps]
 y_test = series[103:, -lookforward]
 
 # %%
-print ('Training Data X Shape : ' + str(X_train.shape))
-print ('Training Data y Shape : ' + str(y_train.shape))
+print("Training Data X Shape : " + str(X_train.shape))
+print("Training Data y Shape : " + str(y_train.shape))
 
-#pure linear prediction
+# pure linear prediction
 np.random.seed(42)
 tf.random.set_seed(42)
 
-model = keras.models.Sequential([
-    keras.layers.Flatten(input_shape=[n_steps, 1]),
-    keras.layers.Dense(1)
-])
+model = keras.models.Sequential([keras.layers.Flatten(input_shape=[n_steps, 1]), keras.layers.Dense(1)])
 
 model.compile(loss="mse", optimizer="adam")
-history = model.fit(X_train, y_train, epochs=20,
-                    validation_data=(X_valid, y_valid))
+history = model.fit(X_train, y_train, epochs=20, validation_data=(X_valid, y_valid))
 
 plot_learning_curves(history.history["loss"], history.history["val_loss"])
 save_fig("simple_linear_rnn_loss_plot")
@@ -158,18 +169,15 @@ y_pred = model.predict(X_valid)
 plot_series(X_valid[0, :, 0], y_valid[0, 0], y_pred[0, 0])
 plt.show()
 
-#Simple RNN
+# Simple RNN
 np.random.seed(42)
 tf.random.set_seed(42)
 
-model = keras.models.Sequential([
-    keras.layers.SimpleRNN(1, input_shape=[None, 1])
-])
+model = keras.models.Sequential([keras.layers.SimpleRNN(1, input_shape=[None, 1])])
 
 optimizer = keras.optimizers.Adam(lr=0.005)
 model.compile(loss="mse", optimizer=optimizer)
-history = model.fit(X_train, y_train, epochs=20,
-                    validation_data=(X_valid, y_valid))
+history = model.fit(X_train, y_train, epochs=20, validation_data=(X_valid, y_valid))
 
 model.evaluate(X_valid, y_valid)
 plot_learning_curves(history.history["loss"], history.history["val_loss"])
@@ -181,19 +189,20 @@ plot_series(X_valid[0, :, 0], y_valid[0, 0], y_pred[0, 0])
 save_fig("simple_rnn_1_step_pred_plot")
 plt.show()
 
-#Deep RNN
+# Deep RNN
 np.random.seed(42)
 tf.random.set_seed(42)
 
-model = keras.models.Sequential([
-    keras.layers.SimpleRNN(20, return_sequences=True, input_shape=[None, 1]),
-    keras.layers.SimpleRNN(20),
-    keras.layers.Dense(1)
-])
+model = keras.models.Sequential(
+    [
+        keras.layers.SimpleRNN(20, return_sequences=True, input_shape=[None, 1]),
+        keras.layers.SimpleRNN(20),
+        keras.layers.Dense(1),
+    ]
+)
 
 model.compile(loss="mse", optimizer="adam")
-history = model.fit(X_train, y_train, epochs=20,
-                    validation_data=(X_valid, y_valid))
+history = model.fit(X_train, y_train, epochs=20, validation_data=(X_valid, y_valid))
 
 model.evaluate(X_valid, y_valid)
 plot_learning_curves(history.history["loss"], history.history["val_loss"])
@@ -206,9 +215,9 @@ save_fig("deep_rnn_1_step_pred_plot")
 plt.show()
 
 
-'''Multi step predictions using RNNs'''
+"""Multi step predictions using RNNs"""
 
-np.random.seed(43) # not 42, as it would give the first series in the train set
+np.random.seed(43)  # not 42, as it would give the first series in the train set
 
 batch_size, series = chop_time_series(df, n_steps + lookforward)
 
@@ -224,16 +233,16 @@ plot_multiple_forecasts(X_new, Y_new, Y_pred)
 save_fig("rnn_multi_step_forecast_ahead_plot")
 plt.show()
 
-'''Now let's create an RNN that predicts the next 10 steps at each time step. 
+"""Now let's create an RNN that predicts the next 10 steps at each time step. 
 That is, instead of just forecasting time steps 50 to 59 based on 
 time steps 0 to 49, it will forecast time steps 1 to 10 at time step 0, then 
 time steps 2 to 11 at time step 1, and so on, and finally it will forecast
  time steps 50 to 59 at the last time step. Notice that the model is 
- causal: when it makes predictions at any time step, it can only see past time steps.'''
+ causal: when it makes predictions at any time step, it can only see past time steps."""
 
 np.random.seed(42)
 
-batch_size, series= chop_time_series(df,n_steps+lookforward)
+batch_size, series = chop_time_series(df, n_steps + lookforward)
 
 X_train = series[:100, :n_steps]
 Y_train = series[:100]
@@ -242,39 +251,44 @@ Y_valid = series[101:103]
 X_test = series[103:, :n_steps]
 Y_test = series[103:]
 Y = np.empty((batch_size, n_steps, lookforward))
-for step_ahead in range (1, lookforward+1):
-    Y[..., step_ahead - 1] = series[..., step_ahead:step_ahead + n_steps, 0]
+for step_ahead in range(1, lookforward + 1):
+    Y[..., step_ahead - 1] = series[..., step_ahead : step_ahead + n_steps, 0]
 
 X_train.shape, Y_train.shape
 
 np.random.seed(42)
 tf.random.set_seed(42)
 
-model = keras.models.Sequential([
-    keras.layers.SimpleRNN(20, return_sequences=True, input_shape=[None, 1]),
-    keras.layers.SimpleRNN(20, return_sequences=True),
-    keras.layers.TimeDistributed(keras.layers.Dense(10)) ])
+model = keras.models.Sequential(
+    [
+        keras.layers.SimpleRNN(20, return_sequences=True, input_shape=[None, 1]),
+        keras.layers.SimpleRNN(20, return_sequences=True),
+        keras.layers.TimeDistributed(keras.layers.Dense(10)),
+    ]
+)
+
 
 def last_time_step_mse(Y_true, Y_pred):
     return keras.metrics.mean_squared_error(Y_true[:, -1], Y_pred[:, -1])
 
-model.compile(loss="mse", optimizer=keras.optimizers.Adam(lr=0.01), metrics=[last_time_step_mse])
-history = model.fit(X_train, Y_train, epochs=20,
-                    validation_data=(X_valid, Y_valid))
 
-#Simple LSTM
+model.compile(loss="mse", optimizer=keras.optimizers.Adam(lr=0.01), metrics=[last_time_step_mse])
+history = model.fit(X_train, Y_train, epochs=20, validation_data=(X_valid, Y_valid))
+
+# Simple LSTM
 np.random.seed(42)
 tf.random.set_seed(42)
 
-model = keras.models.Sequential([
-    keras.layers.LSTM(20, return_sequences=True, input_shape=[None, 1]),
-    keras.layers.LSTM(20, return_sequences=True),
-    keras.layers.TimeDistributed(keras.layers.Dense(lookforward))
-])
+model = keras.models.Sequential(
+    [
+        keras.layers.LSTM(20, return_sequences=True, input_shape=[None, 1]),
+        keras.layers.LSTM(20, return_sequences=True),
+        keras.layers.TimeDistributed(keras.layers.Dense(lookforward)),
+    ]
+)
 
 model.compile(loss="mse", optimizer="adam", metrics=[last_time_step_mse])
-history = model.fit(X_train, Y_train, epochs=20,
-                    validation_data=(X_valid, Y_valid))
+history = model.fit(X_train, Y_train, epochs=20, validation_data=(X_valid, Y_valid))
 
 
 model.evaluate(X_valid, Y_valid)
@@ -293,21 +307,21 @@ plot_multiple_forecasts(X_new, Y_new, Y_pred)
 save_fig("lstm_pred_plot")
 plt.show()
 
-#Conv1D RNN
+# Conv1D RNN
 np.random.seed(42)
 tf.random.set_seed(42)
 
-model = keras.models.Sequential([
-    keras.layers.Conv1D(filters=20, kernel_size=4, strides=2, padding="valid",
-                        input_shape=[None, 1]),
-    keras.layers.GRU(20, return_sequences=True),
-    keras.layers.GRU(20, return_sequences=True),
-    keras.layers.TimeDistributed(keras.layers.Dense(lookforward))
-])
+model = keras.models.Sequential(
+    [
+        keras.layers.Conv1D(filters=20, kernel_size=4, strides=2, padding="valid", input_shape=[None, 1]),
+        keras.layers.GRU(20, return_sequences=True),
+        keras.layers.GRU(20, return_sequences=True),
+        keras.layers.TimeDistributed(keras.layers.Dense(lookforward)),
+    ]
+)
 
 model.compile(loss="mse", optimizer="adam", metrics=[last_time_step_mse])
-history = model.fit(X_train, Y_train[:, 3::2], epochs=20,
-                    validation_data=(X_valid, Y_valid[:, 3::2]))
+history = model.fit(X_train, Y_train[:, 3::2], epochs=20, validation_data=(X_valid, Y_valid[:, 3::2]))
 
 # model.evaluate(X_valid, Y_valid)
 # plot_learning_curves(history.history["loss"], history.history["val_loss"])
@@ -323,5 +337,6 @@ Y_pred = model.predict(X_new)[:, -1][..., np.newaxis]
 plot_multiple_forecasts(X_new, Y_new, Y_pred)
 save_fig("conv_oos_forecast_ahead_plot")
 plt.show()
+
 
 # %%
